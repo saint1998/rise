d3f9316ee857f2766185364eb022ecf39a5b8733
saint1998 <saint1998@yahoo.com>
2021-02-13 14:56:21 +0700

    finish factsheet and companyholder

--- web_scrape.py
+++ web_scrape.py
@@ -23,7 +23,7 @@ def get_all_stock():
       sleep(0.5)
   return stocks
 
-#get auditor, CG
+#get auditor, CG, index, sector
 def get_data_from_factsheet(stocks):
   for stock in stocks:
     url = requests.get("https://www.set.or.th/set/factsheet.do?symbol=%s&ssoPageId=3&language=en&country=US"%urllib.parse.quote(stock['name']))
@@ -33,18 +33,33 @@ def get_data_from_factsheet(stocks):
       soup = BS(url.content, "html.parser")
       auditor = soup.find(lambda tag:tag.name=="strong" and "Auditor" in tag.text).parent.find_next_sibling().find("td").get_text().split("/")[1]
       CG = len(soup.findAll(lambda tag:tag.name=="td" and "CG Report" in tag.text)[-1].findAll('img'))
-      index = soup
+      index = soup.select_one('table > tr:nth-of-type(3) > td > table:nth-of-type(2) > tr:nth-of-type(1) > td > table > tr > td:nth-of-type(1) > table > tr:nth-of-type(2)').text.strip().split(" / ")
+      sector = soup.select_one('table > tr:nth-of-type(3) > td > table:nth-of-type(2) > tr:nth-of-type(1) > td > table > tr > td:nth-of-type(1) > table > tr:nth-of-type(1)').text.strip().split("/")[1]
       stock['auditor'] = auditor
       stock['CG'] = CG
+      stock['index'] = index
+      stock['sector'] = sector
+  return stocks
+
+def get_data_from_companyholder(stocks):
+  for stock in stocks:
+    url = requests.get("https://www.set.or.th/set/companyholder.do?symbol=%s&ssoPageId=6&language=en&country=US"%urllib.parse.quote(stock['name']))
+    status_code = colored(url.status_code,'green') if url.status_code == 200 else colored(url.status_code,'red')
+    print("send request to %s %s"%(url.url,status_code))
+    if url.status_code == 200:
+      soup = BS(url.content, "html.parser")
+      free_float = soup.find(lambda tag:tag.name=="td" and "% Free float" in tag.text).find_next_sibling().text
+      stocks['free_float'] = free_float
+
   return stocks
 
 
 # stocks = get_all_stock()
 # print(get_data_from_factsheet(stocks))
-url = requests.get("https://www.set.or.th/set/factsheet.do?symbol=%s&ssoPageId=3&language=en&country=US"%'A')
+url = requests.get("https://www.set.or.th/set/companyholder.do?symbol=%s&ssoPageId=6&language=en&country=US"%'A')
 status_code = colored(url.status_code,'green') if url.status_code == 200 else colored(url.status_code,'red')
 print(status_code)
 soup = BS(url.content, "html.parser")
-print(soup.select('table > tbody'))
+print(soup.find(lambda tag:tag.name=="td" and "% Free float" in tag.text).find_next_sibling().text)
 # print(url.content)
 # print(soup.find('table').findAll("tr")[1].find("a").get_text())
